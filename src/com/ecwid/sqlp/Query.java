dce4836ebb4112b9ac25903670ab9ca4d7f3176a
ramtary <ramtary@gmail.com>
2021-08-01 20:39:37 +0400

    v1.0

--- src/com/ecwid/sqlp/Query.java
+++ src/com/ecwid/sqlp/Query.java
@@ -3,16 +3,19 @@ package com.ecwid.sqlp;
 import java.util.List;
 
 public class Query {
-    private List<String> columns;
-    private List<Source> fromSources;
-    private List<Join> joins;
-    private List<WhereAndHavingClause> whereAndHavingClauses;
-    private List<String> groupByColumns;
-    private List<Sort> sortColumns;
-    private Integer limit;
-    private Integer offset;
+    private final List<String> columns;
+    private final List<TwinExpr> fromSources;
+    private final List<Join> joins;
+    private final List<WhereAndHavingClause> whereAndHavingClauses;
+    private final List<String> groupByColumns;
+    private final List<TwinExpr> sortColumns;
+    private final Integer limit;
+    private final Integer offset;
+    private final List<Query> subQuery;
 
-    public Query(List<String> columns, List<Source> fromSources, List<Join> joins, List<WhereAndHavingClause> whereAndHavingClauses, List<String> groupByColumns, List<Sort> sortColumns, Integer limit, Integer offset) {
+    public Query(List<String> columns, List<TwinExpr> fromSources, List<Join> joins,
+                 List<WhereAndHavingClause> whereAndHavingClauses, List<String> groupByColumns,
+                 List<TwinExpr> sortColumns, Integer limit, Integer offset, List<Query> subQuery) {
         this.columns = columns;
         this.fromSources = fromSources;
         this.joins = joins;
@@ -21,239 +24,93 @@ public class Query {
         this.sortColumns = sortColumns;
         this.limit = limit;
         this.offset = offset;
+        this.subQuery = subQuery;
     }
 
-    public List<String> getColumns() {
-        return columns;
-    }
-
-    public void setColumns(List<String> columns) {
-        this.columns = columns;
-    }
-
-    public List<Source> getFromSources() {
-        return fromSources;
-    }
-
-    public void setFromSources(List<Source> fromSources) {
-        this.fromSources = fromSources;
-    }
-
-    public List<Join> getJoins() {
-        return joins;
-    }
-
-    public void setJoins(List<Join> joins) {
-        this.joins = joins;
-    }
-
-    public List<WhereAndHavingClause> getWhereAndHavingClauses() {
-        return whereAndHavingClauses;
-    }
-
-    public void setWhereAndHavingClauses(List<WhereAndHavingClause> whereAndHavingClauses) {
-        this.whereAndHavingClauses = whereAndHavingClauses;
-    }
-
-    public List<String> getGroupByColumns() {
-        return groupByColumns;
-    }
-
-    public void setGroupByColumns(List<String> groupByColumns) {
-        this.groupByColumns = groupByColumns;
-    }
-
-    public List<Sort> getSortColumns() {
-        return sortColumns;
-    }
-
-    public void setSortColumns(List<Sort> sortColumns) {
-        this.sortColumns = sortColumns;
-    }
-
-    public Integer getLimit() {
-        return limit;
-    }
-
-    public void setLimit(Integer limit) {
-        this.limit = limit;
-    }
-
-    public Integer getOffset() {
-        return offset;
-    }
-
-    public void setOffset(Integer offset) {
-        this.offset = offset;
-    }
-
-    public static class Source {
-        private String name;
-        private String alias;
-
-        public Source(String name, String alias) {
-            this.name = name;
-            this.alias = alias;
+    @Override
+    public String toString() {
+        String sources = "";
+        for (TwinExpr twinExpr: fromSources) {
+            sources = twinExpr.toString("Источник: " , "Алиас: ");
         }
 
-        public Source(String name) {
-            this.name = name;
-            this.alias = "";
+        String sorts = "";
+        for (TwinExpr twinExpr: sortColumns) {
+            sorts = twinExpr.toString("Поле: " , "Порядок: ");
         }
 
-        public String getName() {
-            return name;
+        String join = "";
+        for (Join expr: joins) {
+            join = expr.toString();
         }
 
-        public void setName(String name) {
-            this.name = name;
+        String whereAndHaving = "";
+        for (WhereAndHavingClause expr: whereAndHavingClauses) {
+            whereAndHaving = expr.toString();
         }
 
-        public String getAlias() {
-            return alias;
+        return "\n" + "Поля: " + columns + "\n\n" +
+                "Источники: " + sources + "\n\n" +
+                "Объединения: " + join + "\n\n" +
+                "Фильтрующие условия: " + whereAndHaving + "\n\n" +
+                "Группировки: " + groupByColumns + "\n\n" +
+                "Сортировки: " + sorts + "\n\n" +
+                "Limit: " + limit + "\n\n" +
+                "Offset: " + offset + "\n\n" +
+                "Подзапросы: " + subQuery + "\n\n";
+    }
+
+    public static class TwinExpr {
+        private final String partOne;
+        private final String partTwo;
+
+        public TwinExpr(String partOne, String partTwo) {
+            this.partOne = partOne;
+            this.partTwo = partTwo;
         }
 
-        public void setAlias(String alias) {
-            this.alias = alias;
+        public TwinExpr(String partOne) {
+            this.partOne = partOne;
+            this.partTwo = "";
+        }
+
+        public String toString(String namePartOne, String namePartTwo) {
+            return "\n" + namePartOne + partOne + "\n"
+                    + namePartTwo + partTwo;
         }
     }
 
-    public static class Join {
-        private String joinType;
-        private String joinTable;
-        private String onFields;
+    public record Join(String joinType, String joinTable, String onFields) {
 
-        public Join(String joinType, String joinTable, String onFields) {
-            this.joinType = joinType;
-            this.joinTable = joinTable;
-            this.onFields = onFields;
-        }
-
-        public String getJoinType() {
-            return joinType;
-        }
-
-        public void setJoinType(String joinType) {
-            this.joinType = joinType;
-        }
-
-        public String getJoinTable() {
-            return joinTable;
-        }
-
-        public void setJoinTable(String joinTable) {
-            this.joinTable = joinTable;
-        }
-
-        public String getOnFields() {
-            return onFields;
-        }
-
-        public void setOnFields(String onFields) {
-            this.onFields = onFields;
+        @Override
+        public String toString() {
+            return "\n" + "Тип: " + joinType + "\n"
+                    + "Таблица/предстваление: " + joinTable + "\n"
+                    + "По полям: " + onFields;
         }
     }
 
-    public static class WhereAndHavingClause {
-        private String fullClause;
-        private List<Clause> clauses;
-        private boolean isHaving;
+    public record WhereAndHavingClause(String fullClause, List<Clause> clauses,
+                                       boolean isHaving) {
 
-        public WhereAndHavingClause(String fullClause, List<Clause> clauses, boolean isHaving) {
-            this.fullClause = fullClause;
-            this.clauses = clauses;
-            this.isHaving = isHaving;
-        }
+        @Override
+        public String toString() {
+            StringBuilder clausesString = new StringBuilder();
+            for (Clause clause : clauses)
+                clausesString.append("\n").append("Условие: ").append(clause.toString());
 
-        public String getFullClause() {
-            return fullClause;
-        }
-
-        public void setFullClause(String fullClause) {
-            this.fullClause = fullClause;
-        }
-
-        public List<Clause> getClauses() {
-            return clauses;
-        }
-
-        public void setClauses(List<Clause> clauses) {
-            this.clauses = clauses;
-        }
-
-        public Boolean getHaving() {
-            return isHaving;
-        }
-
-        public void setHaving(Boolean having) {
-            isHaving = having;
+            return "\n" + "Полное условие: " + fullClause + clausesString + "\n" + "Это Having: " + isHaving;
         }
     }
 
-    public static class Clause {
-        private String field;
-        private String operator;
-        private String value;
+    public record Clause(String field, String operator, String value) {
 
-        public Clause(String field, String operator, String value) {
-            this.field = field;
-            this.operator = operator;
-            this.value = value;
-        }
-
-        public String getField() {
-            return field;
-        }
-
-        public void setField(String field) {
-            this.field = field;
-        }
-
-        public String getOperator() {
-            return operator;
-        }
-
-        public void setOperator(String operator) {
-            this.operator = operator;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value = value;
+        @Override
+        public String toString() {
+            return "\n" + "Поле: " + field + "\n"
+                    + "Оператор: " + operator + "\n"
+                    + "Значение: " + value;
         }
     }
 
-    public static class Sort {
-        private String sortType;
-        private String sortField;
-
-        public Sort(String sortType, String sortField) {
-            this.sortType = sortType;
-            this.sortField = sortField;
-        }
-
-        public Sort(String sortField) {
-            this.sortType = "";
-            this.sortField = sortField;
-        }
-
-        public String getSortType() {
-            return sortType;
-        }
-
-        public void setSortType(String sortType) {
-            this.sortType = sortType;
-        }
-
-        public String getSortField() {
-            return sortField;
-        }
-
-        public void setSortField(String sortField) {
-            this.sortField = sortField;
-        }
-    }
 }
--- src/com/ecwid/sqlp/SQL_Parser.java
+++ src/com/ecwid/sqlp/SQL_Parser.java
@@ -6,18 +6,25 @@ import java.awt.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SQL_Parser {
-    private static final String[] operators = {" = "," > "," < "," >= "," <= "," <> "," != "," !< "," !> "};
+    private static final String[] operators = {"=",">","<",">=","<=","<>","!=","!<","!>"};
 
     public static void main(String[] args) {
+        final String ANSI_RESET = "\u001B[0m";
+        final String ANSI_BLUE = "\u001B[34m";
+        final String ANSI_GREEN = "\u001B[32m";
+
         String queryString = getQueryString();
         Query parsedQuery = parseThis(queryString);
-        System.out.println(parsedQuery);
+        System.out.println(ANSI_GREEN + "Был запрос:" + ANSI_RESET);
+        System.out.println(queryString);
+        System.out.println();
+        System.out.println(ANSI_GREEN + "Получился экземпляр класса Query:" + ANSI_RESET);
+        System.out.println(ANSI_BLUE + parsedQuery + ANSI_RESET);
     }
 
     public static String getQueryString() {
@@ -42,97 +49,111 @@ public class SQL_Parser {
         ArrayList<String> arrayOfQueryLines = new ArrayList<>(queryString.lines().toList());
 
         List<String> columns = new ArrayList<>();
-        List<Query.Source> sources = new ArrayList<>();
+        List<Query.TwinExpr> sources = new ArrayList<>();
         List<Query.Join> joins = new ArrayList<>();
-        List<Query.WhereAndHavingClause> whereAndHavings = new ArrayList<>();
+        List<Query.WhereAndHavingClause> whereAndHaving = new ArrayList<>();
         List<String> groups = new ArrayList<>();
-        List<Query.Sort> sorts = new ArrayList<>();
-        Integer limit = 0, offset = 0;
+        List<Query.TwinExpr> sorts = new ArrayList<>();
+        int limit = 0, offset = 0;
+        List<Query> subQuery = new ArrayList<>();
 
         for (String line: arrayOfQueryLines) {
-            if (line.toUpperCase().contains(KeyWords.SELECT.toString()))
-                extSimple(columns, line, KeyWords.SELECT);
+            line = line.toUpperCase();
 
-            if (line.toUpperCase().contains(KeyWords.FROM.toString()))
-                extSources(sources, line);
+            if (line.contains(KeyWords.SELECT.toString()) & line.indexOf(KeyWords.SELECT.toString()) == 0)
+                extSimple(columns, line, KeyWords.SELECT, subQuery);
 
-            if (line.toUpperCase().contains(KeyWords.JOIN.toString()))
+            if (line.contains(KeyWords.FROM.toString()) & line.indexOf(KeyWords.FROM.toString()) == 0)
+                extTwinExpr(sources, line, KeyWords.FROM);
+
+            if (line.contains(KeyWords.JOIN.toString()))
                 extJoins(joins, line);
 
-            if (line.toUpperCase().contains(KeyWords.WHERE.toString()) | line.toUpperCase().contains(KeyWords.HAVING.toString()))
-                extWhereAndHavings(whereAndHavings, line);
+            if ((line.contains(KeyWords.WHERE.toString()) & line.indexOf(KeyWords.WHERE.toString()) == 0)
+                    | (line.contains(KeyWords.HAVING.toString()) & line.indexOf(KeyWords.HAVING.toString()) == 0))
+                extWhereAndHaving(whereAndHaving, line, subQuery);
 
-            if (line.toUpperCase().contains(KeyWords.GROUP_BY.toString()))
-                extSimple(groups, line, KeyWords.GROUP_BY);
+            if (line.contains(KeyWords.GROUP_BY.toString()) & line.indexOf(KeyWords.GROUP_BY.toString()) == 0)
+                extSimple(groups, line, KeyWords.GROUP_BY, subQuery);
 
-            if (line.toUpperCase().contains(KeyWords.ORDER_BY.toString()))
-                extSorts(sorts, line);
+            if (line.contains(KeyWords.ORDER_BY.toString()))
+                extTwinExpr(sorts, line, KeyWords.ORDER_BY);
 
-            if (line.toUpperCase().contains(KeyWords.LIMIT.toString()))
+            if (line.contains(KeyWords.LIMIT.toString()))
                 limit = extTruncations(line, KeyWords.LIMIT);
 
-            if (line.toUpperCase().contains(KeyWords.OFFSET.toString()))
-                limit = extTruncations(line, KeyWords.OFFSET);
+            if (line.contains(KeyWords.OFFSET.toString()))
+                offset = extTruncations(line, KeyWords.OFFSET);
         }
 
-        return new Query(columns, sources, joins, whereAndHavings, groups, sorts, limit, offset);
+        return new Query(columns, sources, joins, whereAndHaving, groups, sorts, limit, offset, subQuery);
     }
 
-    private static void extWhereAndHavings(List<Query.WhereAndHavingClause> whereAndHavings, String line) {
-        String clearClauseString;
-        boolean isHaving;
+    private static void extWhereAndHaving(List<Query.WhereAndHavingClause> whereAndHaving, String line, List<Query> subQuery) {
+        String clearClauseString = "";
+        boolean isHaving = false;
 
         List<Query.Clause> clauses = new ArrayList<>();
 
-        if (line.toUpperCase().contains(KeyWords.WHERE.toString())) {
-            clearClauseString = removeKeyWord(line.toUpperCase(), KeyWords.WHERE);
-            isHaving = false;
+        if (line.contains(KeyWords.WHERE.toString()) & line.indexOf(KeyWords.WHERE.toString()) == 0) {
+            clearClauseString = removeKeyWord(line, KeyWords.WHERE);
         }
         else {
-            clearClauseString = removeKeyWord(line.toUpperCase(), KeyWords.HAVING);
-            isHaving = true;
+            if (line.indexOf(KeyWords.HAVING.toString()) == 0) {
+                clearClauseString = removeKeyWord(line, KeyWords.HAVING);
+                isHaving = true;
+            }
         }
         String[] clausePartsAND = clearClauseString.split(KeyWords.AND.getWordInQuery());
         String[] clausePartsOR = clearClauseString.split(KeyWords.OR.getWordInQuery());
 
         if (clausePartsAND.length > 1) {
-            extMultiClause(clauses, clausePartsAND, KeyWords.OR);
+            extMultiClause(clauses, clausePartsAND, KeyWords.OR, subQuery);
         }
         else {
             if (clausePartsOR.length > 1)
-                extMultiClause(clauses, clausePartsOR, KeyWords.AND);
+                extMultiClause(clauses, clausePartsOR, KeyWords.AND, subQuery);
             else
-                extSingleClause(clauses, clearClauseString);
+                extSingleClause(clauses, clearClauseString, subQuery);
         }
-        whereAndHavings.add(new Query.WhereAndHavingClause(line.toUpperCase(), clauses, isHaving));
+        whereAndHaving.add(new Query.WhereAndHavingClause(line, clauses, isHaving));
     }
 
-    private static void extMultiClause(List<Query.Clause> clauses, String[] clauseParts, KeyWords keyWord) {
+    private static void extMultiClause(List<Query.Clause> clauses, String[] clauseParts, KeyWords keyWord, List<Query> subQuery) {
         String[] clausePartsDif;
         for (String andClause : clauseParts) {
             andClause = removeBrackets(andClause);
             clausePartsDif = andClause.split(keyWord.getWordInQuery());
             if (clausePartsDif.length > 1) {
                 for (String orClause : clausePartsDif) {
-                    extSingleClause(clauses, orClause);
+                    extSingleClause(clauses, orClause, subQuery);
                 }
             } else {
-                extSingleClause(clauses, andClause);
+                extSingleClause(clauses, andClause, subQuery);
             }
         }
     }
 
-    private static void extSingleClause(List<Query.Clause> clauses, String clause) {
+    private static void extSingleClause(List<Query.Clause> clauses, String clause, List<Query> subQuery) {
+        String[] clauseParts = clause.split(" ");
+
         for (String operator : operators) {
-            if (clause.contains(operator)) {
-                String[] atr = clause.split(operator);
-                clauses.add(new Query.Clause(atr[0], operator, atr[1]));
+            if (clauseParts[1].contains(operator)) {
+                String[] atr = clause.split(" " + operator + " ");
+                if (atr[1].contains(KeyWords.SELECT.toString())) {
+                    clauses.add(new Query.Clause(atr[0], operator, atr[1]));
+                    String query = getCorrectQuery(atr[1]);
+                    subQuery.add(parseThis(query));
+                }
+                else
+                    clauses.add(new Query.Clause(atr[0], operator, atr[1]));
+                break;
             }
         }
     }
 
     private static String removeKeyWord(String line, KeyWords keyWord) {
-        return line.toUpperCase().replace(keyWord.getWordInQuery(), "").replace(";", "");
+        return line.replaceFirst(keyWord.getWordInQuery(), "").replace(";", "");
     }
 
     private static String removeBrackets(String line) {
@@ -142,62 +163,72 @@ public class SQL_Parser {
         return match.find() ? match.group(1) : line;
     }
 
+    private static String removeBracketsAndAlias(String line) {
+        Pattern pattern = Pattern.compile("^\\((.*)\\).*$");
+        Matcher match = pattern.matcher(line);
+
+        return match.find() ? match.group(1) : line;
+    }
+
     private static void extJoins(List<Query.Join> joins, String line) {
-        String joinString = line.toUpperCase();
-        String joinType = joinString.substring(0, line.indexOf(" "));
+        String joinType = line.substring(0, line.indexOf(" "));
 
         switch (joinType) {
-            case "LEFT" -> joinString = removeKeyWord(removeKeyWord(joinString, KeyWords.LEFT), KeyWords.JOIN);
-            case "RIGHT" -> joinString = removeKeyWord(removeKeyWord(joinString, KeyWords.RIGHT), KeyWords.JOIN);
-            case "INNER" -> joinString = removeKeyWord(removeKeyWord(joinString, KeyWords.INNER), KeyWords.JOIN);
+            case "LEFT" -> line = removeKeyWord(removeKeyWord(line, KeyWords.LEFT), KeyWords.JOIN);
+            case "RIGHT" -> line = removeKeyWord(removeKeyWord(line, KeyWords.RIGHT), KeyWords.JOIN);
+            case "INNER" -> line = removeKeyWord(removeKeyWord(line, KeyWords.INNER), KeyWords.JOIN);
             default -> {
-                joinString = removeKeyWord(joinString, KeyWords.JOIN);
+                line = removeKeyWord(line, KeyWords.JOIN);
                 joinType = "";
             }
         }
 
-        String[] joinParts = joinString.split(KeyWords.ON.getWordInQuery());
+        String[] joinParts = line.split(KeyWords.ON.getWordInQuery());
         Query.Join j = new Query.Join(joinType, joinParts[0], joinParts[1]);
         joins.add(j);
     }
 
-    private static void extSources(List<Query.Source> sources, String line) {
-        String sourceString = removeKeyWord(line.toUpperCase(), KeyWords.FROM);
+    private static void extTwinExpr(List<Query.TwinExpr> twinExpr, String line, KeyWords keyWord) {
+        line = removeKeyWord(line, keyWord);
+        Query.TwinExpr extExpr;
 
-        for (String source: sourceString.split(", ")) {
-            Query.Source s;
-            String[] sourceParts = source.split(" ");
+        for (String expr: line.split(", ")) {
+            String[] exprParts = expr.split(" ");
 
-            if (sourceParts.length > 1)
-                s = new Query.Source(sourceParts[0], sourceParts[1]);
+            if (exprParts.length > 1)
+                extExpr = new Query.TwinExpr(exprParts[0], exprParts[1]);
             else
-                s = new Query.Source(source);
-            sources.add(s);
+                extExpr = new Query.TwinExpr(expr);
+            twinExpr.add(extExpr);
         }
     }
 
-    private static void extSorts(List<Query.Sort> sorts, String line) {
-        String sortString = removeKeyWord(line.toUpperCase(), KeyWords.ORDER_BY);
-
-        for (String sort: sortString.split(", ")) {
-            Query.Sort s;
-            String[] sortParts = sort.split(" ");
-
-            if (sortParts.length > 1)
-                s = new Query.Sort(sortParts[1], sortParts[0]);
-            else
-                s = new Query.Sort(sort);
-            sorts.add(s);
+    private static String getCorrectQuery(String query) {
+        KeyWords[] queryKeyWords = {KeyWords.FROM, KeyWords.WHERE, KeyWords.GROUP_BY, KeyWords.HAVING};
+        query = removeBracketsAndAlias(query);
+        for (KeyWords keyWord: queryKeyWords) {
+            query = query.replace(keyWord.toString(), "\r" + keyWord);
         }
+        return query;
     }
 
-    private static void extSimple(List<String> columns, String line, KeyWords keyWord) {
-        String columnsString = removeKeyWord(line.toUpperCase(), keyWord);
-        Collections.addAll(columns, columnsString.split(", "));
+
+    private static void extSimple(List<String> simpleExpr, String line, KeyWords keyWord, List<Query> subQuery) {
+        line = removeKeyWord(line, keyWord);
+        String[] exprParts = line.split(", ");
+
+        for (String expr : exprParts) {
+            if (expr.contains(KeyWords.SELECT.toString())) {
+                simpleExpr.add(expr);
+                expr = getCorrectQuery(expr);
+                subQuery.add(parseThis(expr));
+            }
+            else
+                simpleExpr.add(expr);
+        }
     }
 
     private static Integer extTruncations(String line, KeyWords keyWord) {
-        String truncateString = removeKeyWord(line.toUpperCase(), keyWord);
-        return Integer.getInteger(truncateString);
+        return Integer.parseInt(removeKeyWord(line, keyWord));
     }
 }
